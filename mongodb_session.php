7b213c21c6e941136cf6bb6f5d6890857e7bf797
Mike Preston <mike@technomonk.com>
2011-12-09 17:41:24 +0000

    rewrite of existing code to make it fit session handler model loosly based on code from the php manual...
    mongodb_session.php is the example
    mongodb_session_handler.php is the handler

--- mongodb_session.php
+++ mongodb_session.php
@@ -1,146 +1,30 @@
-<?php 
+<pre><?php 
 /*mongodb custom php session handler
 copyright 2010 ryan day
 modifications by Mike Preston (c) 2011
 lgpl
-
-Changelog:
-fixed session id generator -- MP
-fixed constructor to use configured values -- MP
 */
+
+require_once 'mongodb_session_handler.php';
+
 
 $mongo = new Mongo();
 $mongo_session = new MongoSession($mongo);
+session_start();
 
-
-
-echo $mongo_session->_id."<br/>";
-echo $mongo_session->sid."<br/>";
+echo session_id()."\n";
+echo SID."\n";
 
 
 if(!isset($_SESSION['inc'])) $_SESSION['inc'] = 0;
 
+$_SESSION['date']=gmdate('c');
 $_SESSION['inc']++;
 
 echo "<pre>";
 echo var_export($_SESSION,true);
+$mongo_session->printrow();
 echo "</pre>";
 
-
-
-
-
-
-class mongoSession {
-public static $config = array(
-'db'=>'session',
-'collection'=>'session',
-'expire'=>86400,//day
-'expire_remember'=>2592000//month
-);
-
-public static function setConfig($kv_or_k,$v = null){
-if(!is_array($kv_or_k)){
-$kv_or_k = array($kv_or_k=>$v);
-}
-
-self::$config = array_merge(self::$config,$kv_or_k);
-}
-
-private $collection;
-private $row;
-
-public $_id;
-public $sid;
-
-public function __construct($mongo){
-$db = $mongo->selectDB(self::$config['db']);
-$this->collection = new MongoCollection($db,self::$config['collection']);
-
-$this->row = $this->getRow();
-
-$_SESSION = $this->getData();
-
-$this->_id = $this->row['_id'];
-$this->sid = $this->row['sid'];
-}
-
-public function __destruct(){
-$fields = array("update" => time());
-if(count($_SESSION) || (count($row['data']) && !count($_SESSION)) ){
-$fields["data"] = $_SESSION;
-}
-$new_data = array('$set' => $fields);
-$this->collection->update(array('_id'=>$this->_id),$new_data);
-}
-
-private function getRow(){
-extract($this->cookieVals());
-
-$row = false;
-if($_id){
-$row = $this->collection->findOne(array('_id'=>new MongoId($_id)));
-
-//ensure sid match to pk
-if($row['sid'] != $sid){
-$row = false;
-}
-}
-
-if(!$row){
-$row = array('sid'=>$sid,'update'=>time(),'created'=>time());
-$this->collection->insert($row);
-$_id = $row['_id'];
-}
-
-$this->setCookies($sid,$_id);
-
-return $row;
-}
-
-public function getData(){
-return isset($this->row['data'])?$this->row['data']:array();
-}
-
-private function gc(){
-/*
-session gc...
-capped collection wont work because changing object size and if you ever had over the capped max active/remembered sessions
-capped collection could work if you just stored a refrence to a session_data mongo id and you padded the value with 0's or something so the object would not change size
-
-because this is just as much a lookup table as a write table the index on _id would be useful but with no gc needed you would not need an index on time
-if you wanted to flag session as deactivated you could use the last update
-
-load balanced delete
-every few requests hit mongo with a delete for expired records
-needs index on time
-
-note: if you do a delete for all documents that have an expiry less than current time you will hold a lock while all delete.
-      better to find each and delete one by one
-
-      See cronjob for example
-*/
-}
-
-public function setCookies($sid,$_id){
-setcookie("sid", $sid, time()+self::$config['expire']);
-setcookie("_id", $_id, time()+self::$config['expire']);
-$_COOKIE['sid'] = $sid;
-$_COOKIE['_id'] = $_id.'';//ensure toString is called on MongoId
-}
-
-public function cookieVals(){
-$sid = isset($_COOKIE['sid'])?$_COOKIE['sid']:$this->generate_sid();
-$_id = isset($_COOKIE['_id'])?$_COOKIE['_id']:false;
-//$remember =
-return array('sid'=>$sid,'_id'=>$_id);
-}
-
-public function generate_sid(){
-// mersenne twister pseudo random number generator and microtime together only produce around 30-40 bits of entropy,
-// sha256 is overkill here, but keeps the conspiracy geeks happy - requires PHP > 5.1.2
-return hash('sha256',microtime(TRUE).mt_rand());
-}
-}
-
+//session_destroy();
 ?>
---
+++ mongodb_session_handler.php
@@ -0,0 +1,139 @@
+<?php 
+/*mongodb custom php session handler
+copyright 2010 ryan day
+modifications by Mike Preston (c) 2011
+lgpl
+
+Changelog:
+fixed session id generator -- MP
+fixed constructor to use configured values -- MP
+*/
+
+class mongoSession {
+// 3600 - hour
+// 86400 - day
+// 604800 - week
+// 2419200 - 4 weeks
+// 2592000 - 30 days
+// 2678400 - 31 days
+
+public $config = array(
+	'db'=>'session',
+	'collection'=>'session',
+	'expire'=> 604800, //week
+	'expire_remember'=>2678400, //month - unused in new implementation
+	'cookie_path'   => '/',
+	'cookie_domain' => '', // '' = vhost
+	'session_name' => 'sid',
+	'session_hash' => 'sha224' // 0 - md5, 1 - sha1, sha224, sha256, sha512 etc
+);
+
+public function setConfig($kv_or_k,$v = null){
+	if(!is_array($kv_or_k)){
+		$kv_or_k = array($kv_or_k=>$v);
+	}
+	self::$config = array_merge(self::$config,$kv_or_k);
+}
+
+private $db;
+private $collection;
+private $row;
+public $_id;
+public $sid;
+
+public function __construct($mongo){
+	$this->db = $mongo->selectDB($this->config['db']);
+	$this->collection = new MongoCollection($this->db,$this->config['collection']);
+	
+	ini_set('session.name',			$this->config['session_name']);
+        ini_set('session.auto_start',           0);
+        ini_set('session.gc_probability',       1);
+        ini_set('session.gc_divisor',           1000);
+        ini_set('session.gc_maxlifetime',       $this->config['expire']);
+        ini_set('session.referer_check',        '');
+        ini_set('session.entropy_file',         '/dev/urandom');
+        ini_set('session.entropy_length',       16);
+        ini_set('session.use_cookies',          1);
+        ini_set('session.use_only_cookies',     1);
+        ini_set('session.use_trans_sid',        0);
+        ini_set('session.hash_function',        'sha224');
+        ini_set('session.hash_bits_per_character',  5);
+	ini_set('session.cache_limiter',	'nocache');
+        ini_set('session.cookie_domain',        $this->config['cookie_domain']);
+        ini_set('session.cookie_path',          $this->config['cookie_path']);
+        ini_set('session.cookie_lifetime',	$this->config['expire']);
+
+        session_set_save_handler(
+            array(&$this, 'open'),
+            array(&$this, 'close'),
+            array(&$this, 'read'),
+            array(&$this, 'write'),
+            array(&$this, 'destroy'),
+            array(&$this, 'gc')
+        );
+
+
+}
+
+public function __destruct(){
+}
+
+
+function gc($maxlifetime){
+/*
+session gc...
+load balanced delete
+every few requests hit mongo with a delete for expired records
+needs index on time
+
+note: if you do a delete for all documents that have an expiry less than current time you will hold a lock while all delete.
+      better to find each and delete one by one
+
+      See cronjob/gearman process for example
+      With gearman use coalescing to make sure multiple tasks don't run at once...
+ 
+*/
+}
+
+function printrow() {
+print_r($this->row);
+}
+
+function open($save_path, $session_name){
+//echo "open: $save_path / $session_name\n";
+  return(true);
+}
+
+function close(){
+//echo "close\n";
+  return(true);
+}
+
+function read($id){
+//echo "read: $id\n";
+  $this->row = $this->collection->findOne(array('_id'=>$id));
+  //echo "row : ".print_r($this->row,1);
+  if (!$this->row) {
+    $this->collection->insert(array('_id'=>$id, 'active'=> 1, 'data'=>'', 'expiry'=>(time()+$this->config['expire'])));
+  }
+  //echo "row : ".print_r($this->row,1);
+  return $this->row['data'];
+}
+
+function write($id, $sess_data){
+//echo "write: $id / $sess_data\n";
+$expiry=time()+$this->config['expire'];
+$fields = array('update' => time(),'expiry' => $expiry );
+$fields["data"] = $sess_data;
+$new_data = array('$set' => $fields);
+$this->collection->ensureIndex(array("expiry" => 1)); // not needed *every* time... but on dev systems...
+$this->collection->update(array('_id'=>$id),$new_data);
+}
+
+function destroy($id){
+$this->collection->remove(array('_id'=>$id));
+  return(true);
+}
+
+}
+
